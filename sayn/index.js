bfa5e319d784c7fb9e8e434217cedf14dba7bcbd
wiliiam-code <W689244@gmail.com>
2023-08-09 11:57:31 +0800

    message

--- sayn/index.js
+++ sayn/index.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
                     .text("Result ID " + item.result_UE_BS_info_ID)
             );
         });
+        //                                                                             CIO Table Chart
 
         // Function to display the CIO table
         function displayCIOTable(CIO_table) {
@@ -34,6 +35,23 @@ $(document).ready(function () {
             tableHtml += "</table>";
             $("#cio_table-container").html(tableHtml);
         }
+
+        // Event listener for the resultID_select dropdown
+        $("#resultID_select").on("change", function () {
+            var selectedResultID = $(this).val();
+            var selectedData = data[2].data.find(
+                (item) => item.result_UE_BS_info_ID === selectedResultID
+            );
+
+            var CIO_table = JSON.parse(selectedData.CIO_table);
+            displayCIOTable(CIO_table);
+
+            var ue_init_dist = JSON.parse(selectedData.ue_init_dist);
+            var ue_MLB_dist = JSON.parse(selectedData.ue_MLB_dist);
+            displayBarChart(ue_init_dist, ue_MLB_dist);
+        });
+
+        //                                                                           Bar Chart
 
         // Function to display the bar chart
         function displayBarChart(ue_init_dist, ue_MLB_dist) {
@@ -87,80 +105,81 @@ $(document).ready(function () {
             });
         }
 
-        // Event listener for the resultID_select dropdown
-        $("#resultID_select").on("change", function () {
-            var selectedResultID = $(this).val();
-            var selectedData = data[2].data.find(
-                (item) => item.result_UE_BS_info_ID === selectedResultID
-            );
+        //                                                                            linear Chart
 
-            var CIO_table = JSON.parse(selectedData.CIO_table);
-            displayCIOTable(CIO_table);
+        var dataLabels = ['1s', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s'];
+        var data_init_tp = [133058, 253219, 255194, 133058, 253219, 277318, 277714, 173337, 155194, 177318];
+        var data_MLB_tp = [1338, 33219, 25194, 33058, 25321, 27738, 7714, 17337, 15594, 77318];
 
-            var ue_init_dist = JSON.parse(selectedData.ue_init_dist);
-            var ue_MLB_dist = JSON.parse(selectedData.ue_MLB_dist);
-            displayBarChart(ue_init_dist, ue_MLB_dist);
-        });
+        var config = {
+            type: 'line',
+            data: {
+                labels: dataLabels,
+                datasets: [
+                    {
+                        label: 'Init BS Total Throughput',
+                        data: data_init_tp,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        fill: false,
+                    }
+                    ,
+                    {
+                        label: 'MLB BS Total Throughput',
+                        data: data_MLB_tp,
+                        backgroundColor: 'rgb(0, 0, 255)',
+                        borderColor: 'rgb(0, 0, 255)',
+                        fill: false,
+                    }
+                ]
+
+
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'BS Total Throughput'
+                },
+            }
+        };
+
+        var ctx = document.getElementById('linear_chart-container').getContext('2d');
+        var chart = new Chart(ctx, config);
+
+        function updateChartData() {
+            if (config.data.datasets.length > 0) {
+
+                var last = parseInt(dataLabels[dataLabels.length - 1]);
+                var label = last + 1;
+
+                label = label + 's';
+
+                dataLabels.push(label);
+                data_init_tp.push(getRandomNum(0, 300000));
+                data_MLB_tp.push(getRandomNum(0, 300000));
+
+                dataLabels.shift();
+                data_init_tp.shift();
+                data_MLB_tp.shift();
+
+                chart.update();
+
+                setTimeout(updateChartData, 1000);
+            }
+        }
+
+        updateChartData();
+
+        function getRandomNum(min, max) {
+            var range = max - min;
+            var rand = Math.random();
+            return (min + Math.round(rand * range));
+        }
+
+
 
         // Trigger the change event to display the initial CIO table
         $("#resultID_select").trigger("change");
-
-        // Function to generate a random number between min and max (inclusive)
-        function getRandomNumber(min, max) {
-            return Math.random() * (max - min) + min;
-        }
-
-        // Function to update line chart with random data
-        function updateLineChart() {
-            var bsLabels = ["BS1", "BS2", "BS3", "BS4", "BS5", "BS6", "BS7"];
-            var randomData = bsLabels.map(function () {
-                return getRandomNumber(0, 100); // Generate random data between 0 and 100
-            });
-
-            // Call the function to display the line chart
-            displayLineChart(bsLabels, randomData);
-        }
-
-        // Function to display the line chart
-        function displayLineChart(labels, data) {
-            // Remove any existing chart in the chart-container
-            $("#line_chart-container").empty();
-
-            // Create a canvas element to render the chart
-            var canvas = document.createElement("canvas");
-            canvas.id = "lineChart";
-            $("#line_chart-container").append(canvas);
-
-            var ctx = canvas.getContext("2d");
-
-            new Chart(ctx, {
-                type: "line",
-                data: {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: "Performance",
-                            data: data,
-                            backgroundColor: "rgba(75, 192, 192, 0.5)",
-                            borderColor: "rgba(75, 192, 192, 1)",
-                            borderWidth: 1,
-                            fill: true
-                        }
-                    ]
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false, // Set to true to control the chart's size
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-        }
-
-        // Function to update the line chart every second
-        setInterval(updateLineChart, 1000);
     });
 });
