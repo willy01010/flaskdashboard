e1005f479429c48cfc0f3184fc5b07d5596b8e7e
Lynne613 <acent70@gmail.com>
2023-08-21 16:32:13 +0800

    success create connection to db

---
+++ lynne/php_db/index.php
@@ -0,0 +1,93 @@
+<?php
+/*成功的話會顯示result_UE_BS_info_ID與w2的折線圖
+請負責圖表同學將當前的圖表資料來源換成本程式之作法 
+建議將所有的code都放在這裡
+*/
+
+    /* Database connection settings */
+    include_once 'db.php';
+
+    $data = array();
+
+    $query = "SELECT result_UE_BS_info_ID, w2 FROM mlb_result";
+    $runQuery = mysqli_query($conn, $query);
+
+    while ($row = mysqli_fetch_array($runQuery)) {
+        $data[] = array(
+            'result_UE_BS_info_ID' => $row['result_UE_BS_info_ID'],
+            'w2' => $row['w2']
+        );
+    }
+
+    $json_data = json_encode($data);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+        <title>Line Chart using PHP MySQL and Chart JS</title>
+
+        <style type="text/css">
+            body {
+                font-family: Arial;
+                margin: 80px 100px 10px 100px;
+                padding: 0;
+                color: white;
+                text-align: center;
+                background: #555652;
+            }
+
+            .container {
+                color: #E8E9EB;
+                background: #222;
+                border: #555652 1px solid;
+                padding: 10px;
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>Line Chart using PHP MySQL and Chart JS</h1>
+            <canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+
+            <script>
+                var ctx = document.getElementById("chart").getContext('2d');
+                var data = <?php echo $json_data; ?>;
+                var labels = data.map(item => item.result_UE_BS_info_ID);
+                var w2Data = data.map(item => item.w2);
+
+                var myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: 'w2',
+                                data: w2Data,
+                                backgroundColor: 'transparent',
+                                borderColor: 'rgba(255, 99, 132)',
+                                borderWidth: 3
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{ beginAtZero: false }],
+                            xAxes: [{ autoskip: true, maxTicketsLimit: 20 }]
+                        },
+                        tooltips: { mode: 'index' },
+                        legend: {
+                            display: true,
+                            position: 'top',
+                            labels: { fontColor: 'rgb(255, 255, 255)', fontSize: 16 }
+                        }
+                    }
+                });
+            </script>
+        </div>
+    </body>
+</html>
